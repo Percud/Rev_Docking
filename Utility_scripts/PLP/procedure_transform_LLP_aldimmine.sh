91941e7de487f915c9d902d161f4d911d6b7585b
Riccardo Percudani <riccardo.percudani@gmail.com>
2020-03-09 12:23:29 +0100

    Update procedure_transform_LLP_aldimmine.sh

--- Utility_scripts/PLP/procedure_transform_LLP_aldimmine.sh
+++ Utility_scripts/PLP/procedure_transform_LLP_aldimmine.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 #Replace HETATM for Lys atoms of the internal aldimine 
 
+#Usage procedure_transform_LLP_aldimmine.sh path/*.pdb
+
 for f in $@
 do
   sed -i  's/HETATM\(.*\s*[N|C|O|CA|CB|CG|CE|CD|NZ]\s*\)LLP/ATOM  \1LYS/' $f
