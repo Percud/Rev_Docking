a2b771bc2adb37852aaa92ea8f4a9a165a40c91a
Riccardo Percudani <riccardo.percudani@gmail.com>
2020-03-09 10:33:48 +0100

    Update procedure_get_coord_from_resn.pl

--- Utility_scripts/procedure_get_coord_from_resn.pl
+++ Utility_scripts/procedure_get_coord_from_resn.pl
@@ -38,8 +38,8 @@ while(my $line=<fp>){
   
   my $found=0;
   while(my $pdb_line=<pdb>){
-    if ($pdb_line=~/^ATOM.*$Atom\s.*[A-Z]{3}\s$chain\s*$K_resn\s+(\S+\s+\S+\s+\S+\s+)/){
-        printf "$model $1\n";
+    if ($pdb_line=~/^ATOM.*$Atom\s.*[A-Z]{3}\s$chain\s*$K_resn\s+(-*\d+\.\d+)\s*(-*\d+\.\d+)\s*(-*\d+\.\d+)/){
+        printf "$model $1 $2 $3\n";
         $found=1;
     }
  }
