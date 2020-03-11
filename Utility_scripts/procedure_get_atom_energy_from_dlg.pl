813fefa0e56b2c7f075ae6e7b5a4964828081314
Riccardo Percudani <riccardo.percudani@gmail.com>
2020-03-11 19:23:50 +0100

    Update procedure_get_atom_energy_from_dlg.pl

--- Utility_scripts/procedure_get_atom_energy_from_dlg.pl
+++ Utility_scripts/procedure_get_atom_energy_from_dlg.pl
@@ -20,17 +20,21 @@ printf "Name\tBest\tLARGE\n";
 
 foreach my $f (@F) {
 	my ($clu,@A);
-	my ($large_rank,$large_num);
+	my ($large_rank,$large_num,@large_runs);
     my %E;    
     open T, "<$d/$f" or die $!;
     foreach (<T>){
+    	#CLUSTERING HISTOGRAM
         if (/^\s+(\d+).*\|.*\|\s+(\d+)\s+.*\s+(\S+)\s+\|\#/){
             my ($rank,$run,$num)=($1,$2,$3);
             ($large_rank,$large_num)=($rank,$num) if (!defined $large_num or $num > $large_num);
         }
+
+	#LOWEST ENERGY DOCKED CONFORMATION from EACH CLUSTER
         $clu=$1 if (/Rank = (\d+)/);
         my @A=split /\s+/ if (/^ATOM.*$atom_id/);
         $E{$clu}=0 if (!defined $E{$clu});
+	#Sum vdW and Elec energies for ATOM of ATOM_RES
         $E{$clu}+=$A[-4]+$A[-3] if (defined $A[0]);    
     }     
     printf "$f\t%s\t%s\t(%s)\n", $E{1},$E{$large_rank},$large_num;
