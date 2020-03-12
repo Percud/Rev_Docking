c87e425db91c50faea9fd5f81ba3d030033e2c3c
Riccardo Percudani <riccardo.percudani@gmail.com>
2020-03-12 14:05:20 +0100

    Update procedure_get_atom_energy_from_dlg.pl

--- Utility_scripts/procedure_get_atom_energy_from_dlg.pl
+++ Utility_scripts/procedure_get_atom_energy_from_dlg.pl
@@ -15,29 +15,43 @@ my @F = grep /\.dlg/, readdir (DIR);
 closedir(DIR);
 
 
-printf "Name\tBest\tLARGE\n";
+printf "Name\tBest (run)\tLarge (run)\tBest_Large (run)\t(num)\n";
 
 
 foreach my $f (@F) {
-	my ($clu,@A);
-	my ($large_rank,$large_num,@large_runs);
-    my %E;    
+	my ($run);
+	my ($large_rank,$large_num);
+	my ($best_run, $best_large_run, $best_large_run_atom);
+    my %E; #energy for each run
+    my %C; #cluster for each run
     open T, "<$d/$f" or die $!;
     foreach (<T>){
+    	#FINAL GENETIC ALGORITHM DOCKED STATE
+        $run=$1 if (/Run = (\d+)/);
+        if (/^DOCKED: ATOM.*$atom_id/){
+		my @A=split /\s+/ ;
+        	$E{$run}=0 if (!defined $E{$run});
+		#Sum vdW and Elec energies for ATOM of ATOM_RES
+        	$E{$run}+=$A[-4]+$A[-3] if (defined $A[0]);
+		}
+	
     	#CLUSTERING HISTOGRAM
-        if (/^\s+(\d+).*\|.*\|\s+(\d+)\s+.*\s+(\S+)\s+\|\#/){
+        if (/^\s+(\d+).*\|.*\|\s+\d+\s+.*\s+(\S+)\s+\|\#/){
             my ($rank,$run,$num)=($1,$2,$3);
-            ($large_rank,$large_num)=($rank,$num) if (!defined $large_num or $num > $large_num);
+	    $best_run=$run if (!defined $best_run); #the first listed is best cluster 
+            ($large_rank,$best_large_run,$large_num)=($rank,$run,$num) if (!defined $large_num or $num > $large_num);
         }
+	#RMSD TABLE - cluster for each run
+	if (/^\s+(\d+)\s+\d+\s+(\d+).*RANKING/){
+	$best_large_run_atom=$best_large_run if (!defined $best_large_run_atom);
+		my ($c,$r) = ($1,$2);
+		$best_large_run_atom = $r if ($c==$large_rank and $E{$r}<$E{$best_large_run_atom});
+	}
+	last if (/INFORMATION ENTROPY ANALYSIS FOR THIS CLUSTERING/);
 
-	#LOWEST ENERGY DOCKED CONFORMATION from EACH CLUSTER
-        $clu=$1 if (/Rank = (\d+)/);
-        my @A=split /\s+/ if (/^ATOM.*$atom_id/);
-        $E{$clu}=0 if (!defined $E{$clu});
-	#Sum vdW and Elec energies for ATOM of ATOM_RES
-        $E{$clu}+=$A[-4]+$A[-3] if (defined $A[0]);    
-    }     
-    printf "$f\t%s\t%s\t(%s)\n", $E{1},$E{$large_rank},$large_num;
+    }
+        	
+    printf "$f\t%s ($best_run)\t%s ($best_large_run)\t%s ($best_large_run_atom)\t(%s)\n", $E{$best_run},$E{$best_large_run},$E{$best_large_run_atom},$large_num;
 }
 
 
