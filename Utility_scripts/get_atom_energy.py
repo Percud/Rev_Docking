0227fedaeee1ad0989d358e99c70125770b78f18
marcomala46 <marcomala46@git-hub.com>
2020-11-08 17:05:57 +0100

    added mean columns

--- Utility_scripts/get_atom_energy.py
+++ Utility_scripts/get_atom_energy.py
@@ -12,7 +12,15 @@ except:
     print('USAGE: ./get_atom_energy.py MY_RES_IN_LIGAND MY_DLG_DIRECTORY')
     
 ## print header of table
-print('dlg file', 'Ligand', 'BC (kcal/mol)', 'Run', 'LC (kcal/mol)', 'Run', 'BCaa (kcal/mol)', 'Run', 'LCaa (kcal/mol)', 'Run', 'LCaaB (kcal/mol)', 'Run', 'LCaaM (kcal/mol)', 'BB (kcal/mol)', 'Run', 'LC', 'Num in LC', sep = '\t', file=open(dlg_dir+dlg_dir.split('/')[-2]+'.tsv', 'w')) # tab headers
+print('dlg file', 'Ligand', 
+      'BC (kcal/mol)', 'Run', 'LC (kcal/mol)', 'Run', 
+      'BCaa (kcal/mol)', 'Run', 'LCaa (kcal/mol)','Run', 
+      'BCM (kcal/mol)', 'LCM (kcal/mol)', 
+      'BCaaM (kcal/mol)', 'LCaaM (kcal/mol)',
+      'LC', 'Num in LC', 
+      sep = '\t',
+      file=open(dlg_dir+dlg_dir.split('/')[-2]+'.tsv', 'w')) # tab headers
+
 for dlg in glob.glob(dlg_dir+'/*.dlg'):
     try:
         tab, dock = [], {}
@@ -34,19 +42,31 @@ for dlg in glob.glob(dlg_dir+'/*.dlg'):
         df = df.join(df_dock['vdw+Elec'], on='run').round(2)
         bo = df.groupby('rank')
         bo.idxmin().at[1,'vdw+Elec'][1]
-        best_energy = (df.loc[1].iloc[0]['energy'], df.loc[1]['energy'].idxmin())
-        largest_energy = (df.loc[df['subrank'].idxmax()[0]].iloc[0]['energy'], df.loc[df['subrank'].idxmax()[0]]['energy'].idxmin())
-        best_res = (df.loc[1].iloc[0]['vdw+Elec'], df.loc[1]['subrank'].idxmin())
-        largest_res = (df.loc[df['subrank'].idxmax()[0]].iloc[0]['vdw+Elec'], df.loc[df['subrank'].idxmax()[0]]['subrank'].idxmin())
-        best_largest_res = (df.loc[df['subrank'].idxmax()[0]]['vdw+Elec'].min(), df.loc[df['subrank'].idxmax()[0]]['vdw+Elec'].idxmin())
+        best_energy = (df.loc[1].iloc[0]['energy'], 
+                       df.loc[1]['energy'].idxmin())
+        largest_energy = (df.loc[df['subrank'].idxmax()[0]].iloc[0]['energy'], 
+                          df.loc[df['subrank'].idxmax()[0]]['energy'].idxmin())
+        best_res = (df.loc[1].iloc[0]['vdw+Elec'], 
+                    df.loc[1]['subrank'].idxmin())
+        largest_res = (df.loc[df['subrank'].idxmax()[0]].iloc[0]['vdw+Elec'], 
+                       df.loc[df['subrank'].idxmax()[0]]['subrank'].idxmin())
+        mean_largest = round(df.loc[df['subrank'].idxmax()[0]]['energy'].mean(),2)
+        mean_best = round(df['energy'][1].mean(),2)
+        best_largest_res = (df.loc[df['subrank'].idxmax()[0]]['vdw+Elec'].min(), 
+                            df.loc[df['subrank'].idxmax()[0]]['vdw+Elec'].idxmin())
         mean_largest_res = round(df.loc[df['subrank'].idxmax()[0]]['vdw+Elec'].mean(),2)
+        mean_best_res = round(df['vdw+Elec'][1].mean(),2)
         avg = round(df['vdw+Elec'].mean(),2)
-        bestbest = (df['vdw+Elec'].min(), df['vdw+Elec'].idxmin()[1])
+        bestbest = (df['vdw+Elec'].min(), 
+                    df['vdw+Elec'].idxmin()[1])
         largest_cluster = df['subrank'].idxmax()[0]
         largest_cluster_num = df['subrank'].max()
-        print(os.path.basename(dlg), RES, *best_energy, *largest_energy, *best_res, *largest_res, *best_largest_res, mean_largest_res, *bestbest, largest_cluster, largest_cluster_num, sep = '\t', file=open(dlg_dir+dlg_dir.split('/')[-2]+'.tsv', 'a'))
+        print(os.path.basename(dlg), RES, 
+              *best_energy, *largest_energy, 
+              *best_res, *largest_res, 
+              mean_best, mean_largest,
+              mean_best_res, mean_largest_res,
+              largest_cluster, largest_cluster_num, 
+              sep = '\t', file=open(dlg_dir+dlg_dir.split('/')[-2]+'.tsv', 'a'))
     except:
         print(os.path.basename(dlg), sep = '\t', file=open(dlg_dir+dlg_dir.split('/')[-2]+'.tsv', 'a'))
-        
-
-
